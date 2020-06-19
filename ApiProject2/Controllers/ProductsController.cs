99af81cf60fd4152989f2d1ffa94eac12a63354c
ashish khatiwada <ashishkhatiwada212@gmail.com>
2020-06-20 00:44:07 +0545

    Url Security through Attribute routing . I have applied Int and range on the id attribute in route attribute. which is preventing from accessing other methods in the same controller. without input validation from route there will be injection attacks, which is now prevented.

--- ApiProject2/Controllers/ProductsController.cs
+++ ApiProject2/Controllers/ProductsController.cs
@@ -18,28 +18,35 @@ namespace ApiProject2.Controllers
             return new string[] { "product1 ", "product2" };
         }
 
+
         // GET: api/Products/5
-        [HttpGet,Route("{id}/orders/{custid}")]
-        public string Get(int id,string custid)
+        [HttpGet, Route("{id:int:Range(1000,3000)}")]
+        public string Get(int id)
+        {
+            return "product id = " + id ;
+        }
+        // GET: api/Products/5
+        [HttpGet,Route("{id:int:Range(1000,3000)}/orders/{custid}")]
+        public string GetProductsForCustomer(int id,string custid)
         {
             return "product id = "+id+"custid = "+custid ;
         }
 
         // POST: api/Products
-        [HttpPost,Route("{id}")]
+        [HttpPost,Route("{id:int:Range(1000,3000)}")]
         public void CreateProduct([FromBody]string value)
         {
         }
 
         // PUT: api/Products/5
-        [HttpPut,Route("{id}")]
+        [HttpPut,Route("{id:int:Range(1000,3000)}")]
         public string Put(int id, [FromBody]string value)
         {
             return "returned current person id =" + id + " " + value;
         }
 
         // DELETE: api/Products/5
-        [HttpDelete,Route("{id}")]
+        [HttpDelete,Route("{id:int:Range(1000,3000)}")]
         public string Delete(int id)
         {
             return "deleted value id= " + id;
