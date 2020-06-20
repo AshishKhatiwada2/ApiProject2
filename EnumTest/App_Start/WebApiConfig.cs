e8f27321a30097bcfadcf95d6e83bb779e935860
ashish khatiwada <ashishkhatiwada212@gmail.com>
2020-06-20 09:50:47 +0545

    Removed Previous error by copy and pasting new project into the solution folder only project folder not the solution file. This solves error without doing anything . but if we have two projects on different locations then we should remove EnsureNugetRestore... something like this inside target on the project.config file. and then reloading the project will solve the issue.

--- EnumTest/App_Start/WebApiConfig.cs
+++ EnumTest/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using DeLoachAero.WebApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace EnumTest
 {
@@ -12,13 +14,9 @@ namespace EnumTest
             // Web API configuration and services
 
             // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            var constraintResolver = new DefaultInlineConstraintResolver();
+            constraintResolver.ConstraintMap.Add("enum", typeof(EnumerationConstraint));
+            config.MapHttpAttributeRoutes(constraintResolver);
         }
     }
 }
--- EnumTest/Properties/AssemblyInfo.cs
+++ EnumTest/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EnumTest")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("af550b9c-5491-4f86-ad07-b408f417e1e5")]
+[assembly: Guid("79e0345f-d750-4b51-88d6-9056bb0b85d1")]
 
 // Version information for an assembly consists of the following four values:
 //
